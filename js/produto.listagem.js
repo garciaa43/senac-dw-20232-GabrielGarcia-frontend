f58e915659984b8372c1a8bc5eed8a17de5af4a2
garciaa43 <gabrielcordeir.gg@gmail.com>
2023-09-14 11:23:20 -0300

    Feito filtros

--- js/produto.listagem.js
+++ js/produto.listagem.js
@@ -6,7 +6,28 @@ async function buscarTodosProdutos(){
     });
 }
 
+async function pesquisar() {
+    let options = {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify ({
+            valorMinimo : document.getElementById("valorMin").value,
+            valorMaximo : valorMax.value
+        })
+    };
+
+    const despesaUsuario = await fetch('http://localhost:8080/api/produtos/filtro', options);
+    const despesaJson = await despesaUsuario.json();
+
+    preencherTabela(despesaJson);
+}
+
+function limparTabela(){
+    document.getElementById("corpoTabela").innerHTML = "";
+}
+
 function preencherTabela(jsonProdutos){
+    this.limparTabela();
     // <tr>
     //     <td>1</td>
     //     <td>Café</td>
@@ -26,7 +47,7 @@ function preencherTabela(jsonProdutos){
         celulaNome.innerText = jsonProdutos[i].nome;
 
         let celulaFabricante = novaLinha.insertCell();
-        celulaFabricante.innerText = jsonProdutos[i].fabricanteDoProduto.nome;
+        celulaFabricante.innerText = jsonProdutos[i].fabricante.nome;
 
         let celulaValor = novaLinha.insertCell();
         celulaValor.innerText = 'R$' + jsonProdutos[i].valor;
